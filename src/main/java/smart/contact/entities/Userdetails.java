e60b36a4e81a2f1208104be7b68046069a835377
Anjali Sinha <sinhaanjali82149@gmail.com>
2025-07-14 16:20:23 +0530

    Userdetails.java

--- src/main/java/smart/contact/entities/Userdetails.java
+++ src/main/java/smart/contact/entities/Userdetails.java
@@ -13,8 +13,11 @@ import javax.validation.constraints.Size;
 
 public class Userdetails {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int id;
+	@Column(name="email_id")
+	@NotBlank(message="email can not be blank")
+	@Pattern(regexp="^[a-zA-Z0-9._%=-]+@[a-zA_Z0-9]+\\.[a-zA-Z]{2,6}$", message="please enter a valid email format")
+	private String email;
+	
 	@Column(name="first_name")
 	@NotBlank(message="User name cannot be blank")
 	@Size(min=3, message="User name should be greater than 3")
@@ -23,16 +26,7 @@ public class Userdetails {
 	private String lname;
 	@Column(name="address")
 	private String address;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	@Column(name="email_id")
-	@NotBlank(message="email can not be blank")
-	@Pattern(regexp="^[a-zA-Z0-9._%=-]+@[a-zA_Z0-9]+\\.[a-zA-Z]{2,6}$", message="please enter a valid email format")
-	private String email;
+	
 	@NotBlank(message="mobile number can not be blank")
 	@Size(min=10,max=10,message="mobile number must be exactly 10 digits ")
 	@Column(name="mobile_number")
