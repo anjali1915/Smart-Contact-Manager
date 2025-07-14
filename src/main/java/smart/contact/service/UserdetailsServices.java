4004dd97ab7fe6628c608b8d01ac6a0ecf7c80d4
Anjali Sinha <sinhaanjali82149@gmail.com>
2025-07-14 16:23:06 +0530

    UserdetailsServices.java

--- src/main/java/smart/contact/service/UserdetailsServices.java
+++ src/main/java/smart/contact/service/UserdetailsServices.java
@@ -4,57 +4,54 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import smart.contact.entities.*;
 import smart.contact.dao.UserdetailsRepository;
 
-@Component
+@Service
 public class UserdetailsServices {
-	@Autowired
-	UserdetailsRepository userdetailsRepository;
-	
-	//to display complete data
-	public List<Userdetails> displayAll() {
-		List<Userdetails> list =(List<Userdetails>)this.userdetailsRepository.findAll();
-		return list;
-	}
-	
-	//to create
-	public boolean adduser(Userdetails userData) {
-		Optional<Userdetails> existingUser = userdetailsRepository.findByEmail(userData.getEmail());
-		if(existingUser.isPresent()) {
-			return false;
-		}
-		this.userdetailsRepository.save(userData);
-		return true;
-	}
-	
-	
-	//update 
-	 public void updateData(Userdetails updatedUserData) {
-		 
-		  Optional<Userdetails> existingUser = this.userdetailsRepository.findByEmail(updatedUserData.getEmail());
-		  if(existingUser.isPresent()) {
-			  Userdetails updatedUser = existingUser.get();
-			  updatedUser.setId(updatedUserData.getId()) ;
-			  updatedUser.setFname(updatedUserData.getFname());
-              updatedUser.setLname(updatedUserData.getLname());
-              updatedUser.setAddress(updatedUserData.getAddress());
-              updatedUser.setMobile(updatedUserData.getMobile());
-    		  userdetailsRepository.save(updatedUserData);
-		  }		 
-	 }
-	 
-	//check on the basic of email
-	 public Optional<Userdetails> findByEmail(String email) {
-		 return this.userdetailsRepository.findByEmail(email);
-	 }
-	 
-	//delete user
-	public void deleteuser(int id) {
-		this.userdetailsRepository.deleteById(id);
-	}
+
+    @Autowired
+    private UserdetailsRepository userdetailsRepository;
+
+    // Display all users
+  //to display complete data
+  	public List<Userdetails> displayAll() {
+  		List<Userdetails> list =(List<Userdetails>)this.userdetailsRepository.findAll();
+  		return list;
+  	}
+    // Add user
+    public boolean addUser(Userdetails userData) {
+        Optional<Userdetails> existingUser = userdetailsRepository.findByEmail(userData.getEmail());
+        if (existingUser.isPresent()) {
+            return false;
+        }
+        userdetailsRepository.save(userData);
+        return true;
+    }
+
+    // Update user data
+    public void updateData(Userdetails updatedUserData) {
+        Optional<Userdetails> existingUser = userdetailsRepository.findByEmail(updatedUserData.getEmail());
+        if (existingUser.isPresent()) {
+            Userdetails updatedUser = existingUser.get();
+            updatedUser.setFname(updatedUserData.getFname());
+            updatedUser.setLname(updatedUserData.getLname());
+            updatedUser.setAddress(updatedUserData.getAddress());
+            updatedUser.setMobile(updatedUserData.getMobile());
+            userdetailsRepository.save(updatedUser);
+        }
+    }
+
+    // Find user by email
+    public Optional<Userdetails> findByEmail(String email) {
+        return userdetailsRepository.findByEmail(email);
+    }
+
+    // Delete user
+    public void deleteUser(String email) {
+        if (userdetailsRepository.existsById(email)) {
+            userdetailsRepository.deleteById(email);
+        }
+    }
 }
-
-
-
